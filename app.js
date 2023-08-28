8b7dcddec8edae39599091a640a7a89f7f85e021
“Reza” <reza_sahjpaul@hotmail.com>
2023-08-28 16:59:40 -0700

    commit

--- app.js
+++ app.js
@@ -2,36 +2,41 @@ const grid = document.getElementById("grid");
 const pixelCountButton = document.getElementById("pixelCount");
 const resetButton = document.getElementById("reset");
 
-
 pixelCountButton.addEventListener("click", () => {
-  console.log("hi");
   let count = prompt("How many squares per side? Max is 16");
-  console.log(count);
-  createGrid(count)
+  clearGrid();
+  createGrid(count);
 });
 
-
-const createGrid = (count) => {
-  for (let i = 0; i < (count*count); i++) {
-    let div = document.createElement("div");
-    div.classList.add("pixels");
-    grid.appendChild(div);
-
-    const pixels = document.querySelectorAll(".pixels");
-
-    pixels.forEach((element) => {
-      element.addEventListener("mouseover", () => {
-        // console.log("hi");
-        element.classList.add("hovered");
-      });
-    });
-
-    resetButton.addEventListener("click", () => {
-      pixels.forEach((element) => {
-        element.classList.remove("hovered");
-      });
-    });
+const clearGrid = () => {
+  while (grid.firstChild) {
+    grid.removeChild(grid.firstChild);
   }
 };
 
-createGrid();
+const createGrid = (count) => {
+  if (count > 16 || count < 0) {
+    window.alert("Invalid choice, choose between 0 and 16");
+    return;
+  } else {
+    for (let i = 0; i < count * count; i++) {
+      let div = document.createElement("div");
+      div.classList.add("pixels");
+      grid.appendChild(div);
+
+      const pixels = document.querySelectorAll(".pixels");
+
+      pixels.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+          element.classList.add("hovered");
+        });
+      });
+
+      resetButton.addEventListener("click", () => {
+        pixels.forEach((element) => {
+          element.classList.remove("hovered");
+        });
+      });
+    }
+  }
+};
