bae69552fba561e8040086dfb611825ae1db1a29
“Reza” <reza_sahjpaul@hotmail.com>
2023-08-28 23:17:43 -0700

    commit done

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const pixelCountButton = document.getElementById("pixelCount");
 const resetButton = document.getElementById("reset");
 
 pixelCountButton.addEventListener("click", () => {
-  let count = prompt("How many squares per side? Max is 16");
+  let count = prompt("How many squares per side? Max is 100");
   clearGrid();
   createGrid(count);
 });
@@ -15,28 +15,34 @@ const clearGrid = () => {
 };
 
 const createGrid = (count) => {
-  if (count > 16 || count < 0) {
-    window.alert("Invalid choice, choose between 0 and 16");
+  if (count > 100 || count < 0) {
+    window.alert("Invalid choice, choose between 0 and 100");
     return;
   } else {
+    const gridSize = 560; // Total size of the container
+    const squareSize = ((gridSize / count)-1); // Calculate size of each square
+
     for (let i = 0; i < count * count; i++) {
       let div = document.createElement("div");
       div.classList.add("pixels");
+      div.style.width = `${squareSize}px`;
+      div.style.height = `${squareSize}px`;
+
       grid.appendChild(div);
+    }  
 
-      const pixels = document.querySelectorAll(".pixels");
+    const pixels = document.querySelectorAll(".pixels");
 
+    pixels.forEach((element) => {
+      element.addEventListener("mouseover", () => {
+        element.classList.add("hovered");
+      });
+    });
+
+    resetButton.addEventListener("click", () => {
       pixels.forEach((element) => {
-        element.addEventListener("mouseover", () => {
-          element.classList.add("hovered");
-        });
+        element.classList.remove("hovered");
       });
-
-      resetButton.addEventListener("click", () => {
-        pixels.forEach((element) => {
-          element.classList.remove("hovered");
-        });
-      });
-    }
+    });
   }
 };
